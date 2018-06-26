f41a0df2f2736414120f986a0b9c3cadcb23ceef
kaiserDeng <kaiser_von_d@163.com>
2018-06-27 00:52:29 +0800

    first commit

---
+++ ks-sdk/eureka-client/src/main/java/cn/dengzhk/sdk/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,21 @@
+package cn.dengzhk.sdk.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableDiscoveryClient
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+    {
+
+
+
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
